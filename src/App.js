b00aba4c8d03d9877dd20a0a82b0e925e5dc1e82
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-03-19 10:51:44 +0530

    my-app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
- import logo from './logo.svg';
+//  import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
 
 function App() { 
   return (
-     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with harryfff
+    <>
+   <Navbar title="Agrotech"/>
+  
+    </>
+    /* //  <div className="App">
+    //   <header className="App-header">
+    //  <img src={log/>o} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React with harryfff
 
-        </a>
-      </header>
-    </div>
+    //     </a>
+    //   </header>
+    // </div> */
     
   );
 }
 
 export default App;
+Navbar.propTypes={
+title:Proptypes.string
+}
